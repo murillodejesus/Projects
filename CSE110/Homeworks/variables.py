6b982aaa9ace47a744d6d862a08a9068d831743d
Murillo de Jesus <murillodejesus02@gmail.com>
2026-03-02 22:37:18 -0300

    update on variables

--- CSE110/Homeworks/variables.py
+++ CSE110/Homeworks/variables.py
@@ -1,4 +1,10 @@
-name = "Murillo Lindão"
+name = "Murillo"
 age = 23
+
 print(name)
 print(age)
+
+print(f"Your name is {name}.")
+print(f"Your age is {age}.")
+
+print(f"Hello {name}, you are {age} years old!")
