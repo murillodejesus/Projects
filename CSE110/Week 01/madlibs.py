69f9dd192cf8b661c65b6a8582a80f3f48f05d7e
Murillo de Jesus <murillodejesus02@gmail.com>
2026-03-09 06:47:19 -0300

    update

---
+++ CSE110/Week 01/madlibs.py
@@ -0,0 +1,13 @@
+print ("Please enter the following:/n")
+
+adjective = input("adjective: ")
+animal = input("animal: ")
+verb1 = input("verb: ")
+exclamation = input("exclamation: ")
+verb2 = input("verb: ")
+verb3 = input("verb: ")
+place = input("a place: ")
+
+exclamation_formatted = exclamation.capitalize()
+
+firt
