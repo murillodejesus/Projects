16a568b85be6c4e5a23ac7c943d8cb4c26469e88
Murillo de Jesus <murillodejesus02@gmail.com>
2026-03-11 19:50:20 -0300

    second part

--- CSE110/Week02/numeric_variables.py
+++ CSE110/Week02/numeric_variables.py
@@ -2,4 +2,6 @@ age = int(input(f"How old are you?"))
 next_year = age + 1
 print(f"On your next birthday, you will be {next_year}.")
 
-
+egg = int(input(f"How many egg cartons do you have?"))
+qnt_eggs = egg * 12
+print(f"You have {qnt_eggs} eggs.")
