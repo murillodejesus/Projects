0bb9c89467efcdca04a5e9a311cd600e9e60faa5
Murillo de Jesus <murillodejesus02@gmail.com>
2026-03-11 19:33:20 -0300

    user-input-number

--- CSE110/Week02/user-input.py
+++ CSE110/Week02/user-input.py
@@ -1,6 +1,8 @@
 song1 = 162
 song2 = 175
-song3 = input("Whatis the length of the song?")
+song3 = float(input("Whatis the length of the song?"))
+
+#song3_input = int(song3)
 
 playlist = song1 + song2 + song3
 
