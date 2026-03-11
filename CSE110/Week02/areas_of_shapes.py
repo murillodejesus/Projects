c17451ab48593a878d16de342bde960608fdc55e
Murillo de Jesus <murillodejesus02@gmail.com>
2026-03-11 20:27:17 -0300

    1assiment

---
+++ CSE110/Week02/areas_of_shapes.py
@@ -0,0 +1,3 @@
+square = int(input(f"What is the length of a side of the square?"))
+area = square * 5
+print(f"The area of the Square is: {area:.1f}")
--- CSE110/Week02/numeric_variables.py
+++ CSE110/Week02/numeric_variables.py
@@ -1,10 +1,9 @@
 age = int(input(f"How old are you?"))
 next_year = age + 1
-#print(f"On your next birthday, you will be {next_year}.")
+print(f"On your next birthday, you will be {next_year}.")
 
 egg = int(input(f"How many egg cartons do you have?"))
-qnt_eggs = egg * 12
-#print(f"You have {qnt_eggs} eggs.")
+qnt_eggs = egg * 12#print(f"You have {qnt_eggs} eggs.")
 
 cookies = int(input(f"How many cookies do you have?"))
 people = int(input(f"How many people are there?"))
