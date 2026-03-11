d11abb106886764dc104ed2312f9fe8d914b2791
Murillo de Jesus <murillodejesus02@gmail.com>
2026-03-11 19:54:22 -0300

    third part

--- CSE110/Week02/numeric_variables.py
+++ CSE110/Week02/numeric_variables.py
@@ -1,7 +1,14 @@
 age = int(input(f"How old are you?"))
 next_year = age + 1
-print(f"On your next birthday, you will be {next_year}.")
+#print(f"On your next birthday, you will be {next_year}.")
 
 egg = int(input(f"How many egg cartons do you have?"))
 qnt_eggs = egg * 12
-print(f"You have {qnt_eggs} eggs.")
+#print(f"You have {qnt_eggs} eggs.")
+
+cookies = int(input(f"How many cookies do you have?"))
+people = int(input(f"How many people are there?"))
+
+each = cookies / people 
+
+print(f"Each person may have {each} cookies")
