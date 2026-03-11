02b9d49220583e051a964361194e2cfb3b577f5d
Murillo de Jesus <murillodejesus02@gmail.com>
2026-03-11 20:48:58 -0300

    area_of_shapes_math

--- CSE110/Week02/areas_of_shapes.py
+++ CSE110/Week02/areas_of_shapes.py
@@ -1,3 +1,14 @@
-square = int(input(f"What is the length of a side of the square?"))
+import math
+
+square = int(input(f"What is the length of a side of the square? "))
 area = square * 5
 print(f"The area of the Square is: {area:.1f}")
+
+leng_rectangle = int(input(f"What is the length of rectangle? "))
+widt_rectangle = int(input(f"What is the width of the rectangle? "))
+area = leng_rectangle * widt_rectangle
+print(f"The area of the rectangle is: {area:.1f}")
+
+radius = int(input(f"What is the radius of the circles? "))
+area = math.pi * (radius ** 2)
+print(f"The area of the circle is: {area:.1f}")
