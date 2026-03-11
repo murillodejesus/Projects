46801c96fb16f45bff4d53b615b6753255486a82
Murillo de Jesus <murillodejesus02@gmail.com>
2026-03-11 19:25:05 -0300

    learning numbers

---
+++ CSE110/Week02/datatypes.py
@@ -0,0 +1,10 @@
+song1 = "213 seconds" # this is a string
+song2 = 213 # this is a number
+
+song3 = 213.5 # this is a (float)
+
+song4 = "150"
+song5 = 250.25
+
+playlist = int(song4) + (song5)
+print(f"The playlist is {playlist} seconds.")
