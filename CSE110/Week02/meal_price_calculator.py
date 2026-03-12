798746ccbe791093dee0e6c1b80fdefabac9f67f
Murillo de Jesus <murillodejesus02@gmail.com>
2026-03-11 21:47:08 -0300

    meal_price)calculator

--- CSE110/Week02/meal_price_calculator.py
+++ CSE110/Week02/meal_price_calculator.py
@@ -9,3 +9,17 @@ subtotal = subtotal_children + subtotal_adult
 
 print(f"Subtotal: ${subtotal:.2f}")
 
+tax_rate = float(input(f"What is the sales tax rate?"))
+sales_tax = subtotal * (tax_rate / 100)
+total_price = subtotal + sales_tax
+print(f"Sales Tax: ${sales_tax:.2f}")
+print(f"Total: ${total_price:.2f}")
+
+payment = float(input("What is the payment amount? "))
+change = payment - total_price
+print(f"Change: ${change:.2f}")
+
+print("\n--- Order Summary ---")
+total_people = childrens + adults
+print(f"Total meals served: {total_people}")
+print("Thanks for the order!")
