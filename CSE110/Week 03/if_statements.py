ff5ffdb8ef3b31f9be001c096e30763901a1ce30
Murillo de Jesus <murillodejesus02@gmail.com>
2026-03-19 20:28:53 -0300

    ajustes and add new if statements activity

---
+++ CSE110/Week 03/if_statements.py
@@ -0,0 +1,9 @@
+temperature = float(input ("What is your temperature in degrees Celsius? "))
+if temperature > 40.5:
+    print("Go to the hospital!")
+elif temperature > 39.4:
+    print("Call your Doctor.")
+else:
+    print("Consider rest or medicine.")
+
+print("Have a good day")
