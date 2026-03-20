8ff495053771c2accd5ca035e31a55f8ab38cca4
Murillo de Jesus <murillodejesus02@gmail.com>
2026-03-20 06:24:13 -0300

    applying the boolean

--- CSE110/Week 03/booleanOperators.py
+++ CSE110/Week 03/booleanOperators.py
@@ -3,7 +3,12 @@ women = int(input("How many women are there? "))
 
 total = men + women
 
-if total >= 7 and women >= 4:
+has_enough_players = total >= 7
+has_enough_women = total >=4
+
+
+
+if has_enough_players and has_enough_women:
     print("You can play!")
 else:
     print("You cannot play a lega game.")
