0532641c036f1eabbab4c3b1d95e3b0ea2015062
Murillo de Jesus <murillodejesus02@gmail.com>
2026-03-20 06:28:55 -0300

    finish

--- CSE110/Week 03/booleanOperators.py
+++ CSE110/Week 03/booleanOperators.py
@@ -6,10 +6,14 @@ total = men + women
 has_enough_players = total >= 7
 has_enough_women = total >=4
 
+print(f"We have {total} players")
+
 if has_enough_players and has_enough_women:
     print("You can play!")
 else:
     print("You cannot play a lega game.")
+
+
 
 if not has_enough_players:
     print("Ask the other team if they want to practice")
