e8b336e6feca44dfd430a2a2fc235ac266fcd85e
Murillo de Jesus <murillodejesus02@gmail.com>
2026-03-21 11:23:03 -0300

    update

--- CSE110/Week 03/mistakeswithif.py
+++ CSE110/Week 03/mistakeswithif.py
@@ -1,4 +1,10 @@
-x = int(input("What is the value of x: "))
+#x = int(input("What is the value of x: "))
 
-if x == 5 or x == 6:
-    print("x is 5 or 6")
+#if x == 5 or x == 6:
+#    print("x is 5 or 6")
+#
+
+age = input("What is your age? ")
+age_number = int(age)
+if age_number == 18:
+    print("You're 18!")
