5d7344a071d3f7c888f4e26065ff54b6a3c1d636
Murillo de Jesus <murillodejesus02@gmail.com>
2026-03-21 11:16:04 -0300

    learning

--- CSE110/Week 03/running.py
+++ CSE110/Week 03/running.py
@@ -1,8 +1,17 @@
 temperature = float(input("What is the temperature outseide? "))
 
-if temperature < 5:
+if temperature < -15:
     print("It is too cold. Don't go!")
-elif temperature < -15:
-    print("Ask some other quwestions...")
+elif temperature < 5:
+    weather = input("What is the weather like (snow,rain, sunny)? ")
+
+    if weather == "snow":
+        print("Better stay inside")
+    elif weather == "rain":
+        print("Go ahead and run, but bring rain gear.")
+    elif weather == "sunny":
+        print("Dress warm and enjoy!")
+    else:
+        print("I don't understand that response.")
 else:
     print("Enjoy your run.")
