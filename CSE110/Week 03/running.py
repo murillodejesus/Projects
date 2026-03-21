4ec05a58f5639d02674edc08a0189928eba2695a
Murillo de Jesus <murillodejesus02@gmail.com>
2026-03-21 11:17:23 -0300

    updte

--- CSE110/Week 03/running.py
+++ CSE110/Week 03/running.py
@@ -3,7 +3,7 @@ temperature = float(input("What is the temperature outseide? "))
 if temperature < -15:
     print("It is too cold. Don't go!")
 elif temperature < 5:
-    weather = input("What is the weather like (snow,rain, sunny)? ")
+    weather = input("What is the weather like (snow, rain, sunny)? ")
 
     if weather == "snow":
         print("Better stay inside")
