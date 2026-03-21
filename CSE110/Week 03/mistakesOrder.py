19a36470d24fb45cdec2c05ba6f194dec841b045
Murillo de Jesus <murillodejesus02@gmail.com>
2026-03-21 11:57:25 -0300

    ajuste

--- CSE110/Week 03/mistakesOrder.py
+++ CSE110/Week 03/mistakesOrder.py
@@ -2,7 +2,7 @@ rewards = 0
 choice = "drink"
 total_order = 3
 
-if choice == "drink" or choice == "cookie":
+if (choice == "drink" or choice == "cookie") and total_order > 5:
     rewards += 5
 
 print(f"You have {rewards} rewards points.")
