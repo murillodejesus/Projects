56633d4d9d12cf5668f59285c81ca373a7dda526
Murillo de Jesus <murillodejesus02@gmail.com>
2026-03-23 07:38:21 -0300

    guessnumber

--- CSE110/Week 04/guessnumber.py
+++ CSE110/Week 04/guessnumber.py
@@ -1,5 +1,29 @@
 import random
 
-number = random.randint(1, 10)
+keep_playing = "yes"
 
-print(number)
+while keep_playing == "yes":
+    magic_number = random.radiant(1, 100)
+
+    guess_count = 0
+
+    guess = -1
+
+    while guess != magic_number:
+        guess = int(input("What is your guess? "))
+        guess_count = guess_count + 1
+
+        if guess < magic_number:
+            print("Higher")
+        elif guess > magic_number:
+            print("Lower")
+        else:
+            print("You guessed it!")
+
+    print(f"It took you {guess_count} guesses")
+    
+    print()
+
+    keep_playing = input("Would you like to play agian (yes/no?) ")
+
+print("Thank you for playing. Goodbye")
