c3c2d4839390ce7177b05b4db0eeb180b199d4bf
Murillo de Jesus <murillodejesus02@gmail.com>
2026-04-03 19:55:14 -0300

    ajustado o código com erro na sintax de secret_word

--- CSE110/Week 04/finalwordpuzzle.py
+++ CSE110/Week 04/finalwordpuzzle.py
@@ -5,7 +5,7 @@ play_again = "yes"
 
 print("Welcome to the word guessing game!")
 
-while play_again.lower() != "yes":
+while play_again.lower() == "yes":
     secret_word = random.choice(words) 
     guess = ""
     count = 0
@@ -18,11 +18,11 @@ while play_again.lower() != "yes":
 
         if len(guess) != len(secret_word):
             print(f"Sorry, the guess must have {len(secret_word)} letters.")
-
-        if guess != secret_word:
+        
+        elif guess != secret_word:
             hint = ""
             for i in range(len(secret_word)):
-                if guess[i] != secret_word[i]:
+                if guess[i] == secret_word[i]:
                     hint += guess[i].upper() + " "
                 elif guess[i] in secret_word:
                     hint += guess[i].lower() + " "
