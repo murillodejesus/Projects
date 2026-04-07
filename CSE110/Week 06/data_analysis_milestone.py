1134c2ac29206b23d1901ccd7e06c9f291048f49
Murillo de Jesus <murillodejesus02@gmail.com>
2026-04-07 07:28:45 -0300

    milestone project

---
+++ CSE110/Week 06/data_analysis_milestone.py
@@ -0,0 +1,20 @@
+min_life_expectancy = 9999.0
+max_life_expectancy = -1.0
+
+with open("week 06/life-expectancy.csv") as data_file:
+    next(data_file)
+    
+    for line in data_file:
+        parts = line.strip().split(",")
+        
+        life_expectancy = float(parts[3])
+        
+        if life_expectancy > max_life_expectancy:
+            max_life_expectancy = life_expectancy
+            
+        if life_expectancy < min_life_expectancy:
+            min_life_expectancy = life_expectancy
+
+# Display the results for the milestone
+print(f"The overall max life expectancy is: {max_life_expectancy}")
+print(f"The overall min life expectancy is: {min_life_expectancy}")
